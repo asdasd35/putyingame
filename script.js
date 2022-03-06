1d28ab2541810130515a6d4093a54610958b0c35
asdasd35 <88936560+asdasd35@users.noreply.github.com>
2022-03-06 10:48:31 +0100

    asd

---
+++ script.js
@@ -0,0 +1,21 @@
+const putyin1 = document.getElementById('p1');
+const putyin2 = document.getElementById('p2');
+const putyin3 = document.getElementById('p3');
+
+function p1c() {
+    putyin3.style.backgroundImage = "url('putyin.png')";
+    putyin1.style.backgroundColor = "steelblue";
+    putyin1.style.backgroundImage = "unset"
+}
+
+function p3c() {
+    putyin2.style.backgroundImage = "url('putyin.png')";
+    putyin3.style.backgroundColor = "steelblue";
+    putyin3.style.backgroundImage = "unset"
+}
+
+function p2c() {
+    putyin1.style.backgroundImage = "url('putyin.png')";
+    putyin2.style.backgroundColor = "steelblue";
+    putyin2.style.backgroundImage = "unset"
+}
